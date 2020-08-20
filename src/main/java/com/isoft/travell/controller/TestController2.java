9476a901ef80dfc22ee467beaf13d469f2b491c9
leiliu <1935314139@qq.com>
2020-08-20 10:30:51 +0800

    这是新加的TestController2

---
+++ src/main/java/com/isoft/travell/controller/TestController2.java
@@ -0,0 +1,8 @@
+package com.isoft.travell.controller;
+
+public class TestController2 {
+    public static void main(String[] args) {
+        System.out.println("马国瑞" +
+                "刘磊");
+    }
+}
